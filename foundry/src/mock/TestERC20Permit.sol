5e3d7bf58263f17952e71e480471af2aec7a1ca6
paritomarrr <pari2019cse083abesit@gmail.com>
2025-05-20 20:04:53 +0530

    Added Foundry test cases

--- foundry/src/mock/TestERC20Permit.sol
+++ foundry/src/mock/TestERC20Permit.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC20Permit} from "../ERC20Permit.sol";
 import {ERC20} from "../helper/ERC20.sol";
 
-contract TestERC20Permit is ERC20Permit {
+contract MockERC20Permit is ERC20Permit {
     constructor(
         string memory name,
         string memory symbol
